d4e25f53fbac1e4a4b1389f17190c4af7c4ea34d
Daniella Mesquita <daniellmesquita@protonmail.com>
2022-03-18 11:20:55 -0300

    About window: add logo, message that updates system isn't available and working/updated Discord link

--- browser/renderer/components/about/about.js
+++ browser/renderer/components/about/about.js
@@ -3,14 +3,15 @@ let core = require('../../../system_assets/modules/OhHaiBrowser.Core');
 module.exports = function load(){
 	let aboutPage = core.generateElement(`
 	<div class='SettingsList'>
+	        <center><img width="128px" src="../assets/imgs/logo.png"></center>
 		<p>Browser Version</p>
 		<span>${window.OhHaiBrowser.version}</span>
 
 		<p>Update Status</p>
-		<span>Your browser is upto date :)</span>
+		<span>Update system isn't available, yet :(</span>
 
 		<p>Join the community</p>
-		<a href='javascript:window.OhHaiBrowser.tabs.activePage.navigate("https://discord.gg/Q8QAdTs");'>Discord server</a>
+		<a href='javascript:window.OhHaiBrowser.tabs.activePage.navigate("https://discord.gg/DFYadQmv3E");'>Discord server</a>
 
 	</div>`);
 
